789d0b767e1ea780d523d056e8c0491737e5d721
Jose Mieses <jairo20088@gmail.com>
2020-05-17 03:18:19 -0500

    added about heading, big-splash and text

--- src/containers/about/about.js
+++ src/containers/about/about.js
@@ -1,6 +1,8 @@
 import React from "react";
 import style from "./about.module.css";
 import Splash from "../../assets/splash.png";
+import video from "../../assets/Aqua-Intro.mp4";
+import BigSplash from "../../assets/big-splash-2.png";
 const About = props => {
   return (
     <section className={style.about}>
@@ -12,7 +14,33 @@ const About = props => {
       <div className={style.smallSplash}>
         <img src={Splash} />
       </div>
-      <div className={style.aboutVideo}></div>
+      <div className={style.aboutVideo}>
+        <video
+          src={video}
+          controls="controls"
+          muted
+          //autoplay="true"
+        />
+      </div>
+      <div className={style.aboutContainer}>
+        <div className={style.aboutTitle}>
+          <h3>About us</h3>
+        </div>
+        <div>
+          <p>
+            Team Aqua is an upcoming eSports team based in Curaçao Our main goal
+            is to help find talented players and open opportunities for them.
+            Our community is the most important aspect of the team. The amount
+            of support that were recently receiving is tremendous and is a
+            stimulant for us to keep working harder on the team. We organize
+            scrims and tournaments on a weekly basis so we can promote hidden
+            talent and give back to the community.
+          </p>
+        </div>
+      </div>
+      <div className={style.bigSplash}>
+        <img src={BigSplash} />
+      </div>
     </section>
   );
 };
